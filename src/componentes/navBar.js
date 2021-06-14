049d5d68ac1f6a077fdf189e6d0d2f7529005d51
YionPasteleria <nahuelcr.lopez@gmail.com>
2021-06-14 12:54:00 -0300

    Menu E-commerce

---
+++ src/componentes/navBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+      <header>
+            <a href="">Portfolio</a>
+              <ul>
+                  <li>Inicio</li>
+                  <li>Categoria</li>
+                  <li>Contacto</li>
+              </ul>
+      </header>
+    )
+}
